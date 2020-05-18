c5c5f9da02870773aa8afe9ac8dac5f8f0f6147b
Jegede Olatide <60626256+Henryola05@users.noreply.github.com>
2020-05-18 13:40:47 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+const input = 'You are not my mon';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+for (let i = 0; i < input.length; i++) {
+//console.log('Input index = ' + input[i]);
+for (let j = 0; j < vowels.length; j++){
+  console.log('vowel = ' + j);
+  if (input[i] === vowels[j]) {
+if (input[i] === 'e'){
+resultArray.push('ee');
+  } else if (input[i] === 'u'){
+resultArray.push('uu');
+  }
+  else {
+    resultArray.push(input[i]);
+  }
+  }
+  }
+}
+console.log(resultArray.join('').toUpperCase());
